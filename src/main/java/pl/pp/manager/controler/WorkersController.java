53871ef450bc2de9e105962316f71a1b5009d1c7
Tomasz Kaczorkiewicz <bossandmaster69@gmail.com>
2022-04-19 01:38:50 +0200

    Add worker controller

---
+++ src/main/java/pl/pp/manager/controler/WorkersController.java
@@ -0,0 +1,22 @@
+package pl.pp.manager.controler;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/list")
+public class WorkersController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/worker/list.jsp");
+        dispatcher.forward(request, response);
+
+    }
+}
