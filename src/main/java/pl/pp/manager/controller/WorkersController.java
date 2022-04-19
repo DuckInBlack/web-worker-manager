51f0a1dee9acbd322141dc7232bfbd0b3a17823c
Tomasz Kaczorkiewicz <bossandmaster69@gmail.com>
2022-04-19 22:58:08 +0200

    Add connect to DB

--- src/main/java/pl/pp/manager/controller/WorkersController.java
+++ src/main/java/pl/pp/manager/controller/WorkersController.java
@@ -6,8 +6,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import pl.pp.manager.model.Worker;
+import pl.pp.manager.model.WorkersDBUtils;
 
 import java.io.IOException;
+import java.util.List;
 
 
 @WebServlet("/list")
@@ -15,6 +18,8 @@ public class WorkersController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        List<Worker> workers = WorkersDBUtils.getWorkers();
+        request.setAttribute("workers", workers);
         RequestDispatcher dispatcher = request.getRequestDispatcher("/worker/list.jsp");
         dispatcher.forward(request, response);
 
