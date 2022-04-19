781a0cc63e39fed1fa3b0695b1568e9d082ec2d0
Tomasz Kaczorkiewicz <bossandmaster69@gmail.com>
2022-04-19 23:47:39 +0200

    Configure DataBase connection pool

--- src/main/java/pl/pp/manager/controller/WorkersController.java
+++ src/main/java/pl/pp/manager/controller/WorkersController.java
@@ -9,16 +9,22 @@ import jakarta.servlet.http.HttpServletResponse;
 import pl.pp.manager.model.Worker;
 import pl.pp.manager.model.WorkersDBUtils;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.List;
 
 
 @WebServlet("/list")
 public class WorkersController extends HttpServlet {
+
+    @Resource(name = "web-worker-manager-db")
+    private DataSource dataSource;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        List<Worker> workers = WorkersDBUtils.getWorkers();
+        List<Worker> workers = WorkersDBUtils.getWorkers(dataSource);
         request.setAttribute("workers", workers);
         RequestDispatcher dispatcher = request.getRequestDispatcher("/worker/list.jsp");
         dispatcher.forward(request, response);
